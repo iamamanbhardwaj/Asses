afd38d6cb3c4027c2767d7f66c8e76db0e5180a9
aditya-malik429 <60377203+aditya-malik429@users.noreply.github.com>
2022-04-29 14:55:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace clunk {
+    public static class extension {
+        public static bool IsEven(this int x) {
+            if(x%2==0)
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    class Program {
+        static void Main() {
+            int number=10;
+            Console.WriteLine(number.IsEven());
+        }
+    }
+}
